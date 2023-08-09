eb3f11cab11b5bcbba680715f939c8eeedf6a433
Ilia <ilya2491@gmail.com>
2023-08-09 13:50:14 +0400

    Add password unlocker with indicator

---
+++ iOS10-HW17-Bessonov Ilia/Extensions/StringPassword.swift
@@ -0,0 +1,8 @@
+//
+//  StringPassword.swift
+//  iOS10-HW17-Bessonov Ilia
+//
+//  Created by i0240 on 08.08.2023.
+//
+
+import Foundation
---
+++ iOS10-HW17-Bessonov Ilia/Extensions/UIView+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Extensions.swift
+//  iOS10-HW17-Bessonov Ilia
+//
+//  Created by i0240 on 07.08.2023.
+//
+
+import Foundation
